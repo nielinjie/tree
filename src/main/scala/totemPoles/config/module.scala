be0e4a4b4eeecd1516c9b2f503834a2a3f638de0
nielinjie <nielinjie@gmail.com>
2014-10-20 09:12:00 +0800

    sbt web plugin upgraded.

--- src/main/scala/totemPoles/config/module.scala
+++ src/main/scala/totemPoles/config/module.scala
@@ -4,12 +4,7 @@ import com.escalatesoft.subcut.inject.BindingId
 
 object BindingKeys {
 
-  // in some other file?
-  object DataRepositoryId extends BindingId
 
-  object ChartRepositoryId extends BindingId
-
-  object DataPlanId extends BindingId
 
   object AllPlanId extends BindingId
 
